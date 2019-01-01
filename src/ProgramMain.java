5059f17bdade74eed691ce2d9bb9b69e9810cec4
cristicojo <crii_stii@yahoo.com>
2019-01-01 22:37:03 +0200

    quicksort

---
+++ src/ProgramMain.java
@@ -0,0 +1,46 @@
+import static java.lang.Math.random;
+
+import java.util.Scanner;
+
+public class ProgramMain {
+
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+
+        //array dat de user
+        //int input[]={24,2,45,20,56,75,2,56,99,53};
+        //int input[]={};
+
+        int input[] = new int[10];
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < input.length; i++) {
+            //array dat de la tastatura
+            //System.out.print("input[" + i + "]=");
+            //input[i] = s.nextInt();
+
+            //random array
+            input[i] = (int) (random() * 100);
+        }
+
+
+        System.out.print("inainte de sortare:");
+        for (int z : input)
+            System.out.print(z + " ");
+
+        qs.callquicksort(input);
+
+        System.out.println();
+
+        System.out.print("dupa sortare array descrescator:");
+        for (int i = input.length - 1; i >= 0; i--) {
+            System.out.print(input[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("dupa sortare array crescator:");
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+
+    }
+}
---
+++ src/QuickSort.java
@@ -0,0 +1,72 @@
+public class QuickSort {
+
+    private int length;
+    private int array[];
+
+    int callquicksort(int userInput[]) {
+
+        //verificam sa nu avem array fara elem sau lungimea array sa nu fie 0
+        if (userInput == null || userInput.length == 0) {
+            return 1;
+        }
+
+        this.array = userInput;
+        this.length = userInput.length;
+
+        quicksort(0, length - 1);
+        return 0;
+    }
+
+    public void quicksort(int lowerIndex, int higherIndex) {
+
+
+        int indexMic = lowerIndex;
+        int indexMare = higherIndex;
+        //alegem pivotul sa fie elementul al carui index este din mijlocul array
+        int pivot = array[indexMic + (indexMare - indexMic) / 2];
+
+        //impartim array mare in doua array mici
+        while (indexMic <= indexMare) {
+
+            /**
+             In each iteration, we will identify a number from left side which
+             is greater then the pivot value, and also we will identify a number
+             from right side which is less then the pivot value. Once the search
+             is done, then we exchange both numbers.
+             */
+
+            while (array[indexMic] < pivot) {
+                indexMic++;
+            }
+
+            while (array[indexMare] > pivot) {
+                indexMare--;
+            }
+
+            //facem schimbul de elem
+            if (indexMic <= indexMare) {
+                exchangeNumbers(indexMic, indexMare);
+                //mutam indexul pe urmatoarea pozitie in ambele parti
+                indexMic++;
+                indexMare--;
+            }
+        }
+
+        //apelam metoda quicksort() recursiv pt subarray din stanga pivotului respectiv dreapta lui
+        if (lowerIndex < indexMare) {
+            quicksort(lowerIndex, indexMare);
+        }
+        if (higherIndex > indexMic) {
+            quicksort(indexMic, higherIndex);
+        }
+    }
+
+    public void exchangeNumbers(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+}
+
